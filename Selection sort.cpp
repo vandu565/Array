b5bb4be92df0ccbb5976d502177cbf5ec5c22eb4
Vandana Yadav <67888275+vandu565@users.noreply.github.com>
2020-10-10 18:29:50 +0530

    Create Selection sort.cpp

---
+++ Selection sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void  selectionSort(int a[],int n){
+    for(int i=0;i<=n-2;i++){
+    int smallest = i;
+
+    //Find the smallest element
+    for(int j=i+1;j<n;j++){
+            if(a[j]<a[smallest]){
+                smallest = j;
+            }
+    }
+    int temp = a[i];
+    a[i] = a[smallest];
+    a[smallest] = temp;
+   }
+
+}
+
+int main(){
+int a[100];
+int n;
+cin>>n;
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+selectionSort(a,n);
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+return 0;}
